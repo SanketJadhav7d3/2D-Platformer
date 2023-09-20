100caa0a8a58a4411f1c164435446a1c63ff9289
SanketJadhav7d3 <sanketjadhav7d3@gmail.com>
2023-09-20 17:10:53 +0530

    all side collision

--- src/level.js
+++ src/level.js
@@ -55,13 +55,15 @@ class Level {
 //      }
 //    }
 //
-
   }
 
   collision({object1, object2}) {
     return (
-      object1.position.y + object1.dimension.height >= object2.position.y 
-    )
+      object1.position.y + object1.dimension.height >= object2.position.y &&
+      object1.position.y <= object2.position.y + object2.dimension.height &&
+      object1.position.x + object1.dimension.width >= object2.position.x &&
+      object1.position.x <= object2.position.x + object2.dimension.width
+    );
   }
 
   drawBackground() {
